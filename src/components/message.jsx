832e22827e8455b335098f9879acf157fbd9b2d0
Sharanya <sharushri@users.noreply.github.com>
2019-02-07 23:23:53 +0530

    added

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -2,96 +2,101 @@ import React, { Component } from "react";
 import "./rental.css";
 import styled from "styled-components";
 
-const Input = styled.input`
-  margin-top: 25px;
-  border-radius: 15px;
-  border: 1px dotted white;
-  width: 30%;
-  height: 40px;
-  margin-left: 5%;
-  margin-bottom: 20px;
-  background-color: DodgerBlue;
-  font-size: 20px;
-  color: rgba(255, 255, 255, 0.7);
-`;
+const StyledMessage = styled.div`
+  input[type="submit"] {
+    margin-top: 25px;
+    border-radius: 15px;
+    border: 1px dotted white;
+    width: 30%;
+    height: 40px;
+    margin-left: 5%;
+    margin-bottom: 20px;
+    background-color: DodgerBlue;
+    font-size: 20px;
+    color: rgba(255, 255, 255, 0.7);
+  }
 
-const Msg = styled.p`
-  font-family: "Monoton", cursive;
-  color: rgb(238, 135, 110);
-  background: none;
-  border: none;
-  border-left: 3px #eee solid;
-  font-size: 30px;
-  margin-bottom: 30px;
-  margin-left: 4%;
-`;
+  .msg-head {
+    font-family: "Monoton", cursive;
+    color: rgb(238, 135, 110);
+    background: none;
+    border: none;
+    border-left: 3px #eee solid;
+    font-size: 30px;
+    margin-bottom: 30px;
+    margin-left: 4%;
+  }
 
-const TextBox = styled.input`
-  border: 2px #fff ridge;
-  outline: none;
-  border-radius: 20px;
-  background-color: rgba(180, 180, 180, 0.4);
-  box-sizing: border-box;
-  font-size: 16px;
-  padding: 20px;
-  color: #fff;
-  font-family: "Montserrat", sans-serif;
-  margin-left: 4%;
-  width: 90%;
-  margin-top: 20px;
+  input[type="text"],
+  textarea {
+    border: 2px #fff ridge;
+    outline: none;
+    border-radius: 20px;
+    background-color: rgba(180, 180, 180, 0.4);
+    box-sizing: border-box;
+    font-size: 16px;
+    padding: 20px;
+    color: #fff;
+    font-family: "Montserrat", sans-serif;
+    margin-left: 4%;
+    width: 90%;
+    margin-top: 20px;
+  }
 `;
 
 class Message extends Component {
   render() {
     return (
-      <div className="message-box-section">
-        <div className="message-box">
-          <form>
-            <Msg>get in touch</Msg>
-            <div className="actual-form">
-              <div className="first">
+      <StyledMessage>
+        <div className="message-box-section">
+          <div className="message-box">
+            <form>
+              <p className="msg-head">get in touch</p>
+              <div className="actual-form">
+                <div className="first">
+                  <div className="Input_Field">
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Your name here"
+                    />
+                  </div>
+                  <div className="Input_Field">
+                    <input
+                      type="text"
+                      name="phone"
+                      placeholder="Your phone here"
+                    />
+                  </div>
+                </div>
                 <div className="Input_Field">
-                  <TextBox
+                  <input
                     type="text"
-                    name="name"
-                    placeholder="Your name here"
+                    name="email"
+                    placeholder="Your email here"
                   />
                 </div>
                 <div className="Input_Field">
-                  <TextBox
+                  <input
                     type="text"
-                    name="phone"
-                    placeholder="Your phone here"
+                    name="subject"
+                    placeholder="Subject here"
                   />
                 </div>
+                <div className="Input_Field">
+                  <textarea
+                    className="custom-textarea"
+                    name="message"
+                    placeholder="Message"
+                  />
+                </div>
+                <input type="submit" />
               </div>
-              <div className="Input_Field">
-                <TextBox
-                  type="text"
-                  name="email"
-                  placeholder="Your email here"
-                />
-              </div>
-              <div className="Input_Field">
-                <TextBox
-                  type="text"
-                  name="subject"
-                  placeholder="Subject here"
-                />
-              </div>
-              <div className="Input_Field">
-                <TextBox
-                  className="custom-textarea"
-                  name="message"
-                  placeholder="Message"
-                />
-              </div>
-              <Input type="submit" />
-            </div>
-          </form>
-          <p className="form-message" />
+            </form>
+            <p className="form-message" />
+          </div>
         </div>
-      </div>
+      </StyledMessage>
     );
   }
 }
